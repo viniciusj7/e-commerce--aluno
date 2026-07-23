d735810a51263be45e0624942db63b96097a82c1
Marcos Vinicius <viniciusjunior4444@gmail.com>
2026-07-23 17:27:56 -0300

    chore: nova pastar

---
+++ src/app/features/produtos/produto/produtos.service.ts
@@ -0,0 +1,28 @@
+import { HttpClient } from "@angular/common/http";
+import { inject, Injectable } from "@angular/core";
+import { Inject } from "@angular/core";
+ type ProdutoApi ={
+    title: string ;
+    price: number ;
+ };
+ type Produto = {
+    nome: string;
+    preco: number;
+ };
+ @Injectable({providedIn: 'root'})
+ export class produtoService {
+  private http = inject(HttpClient); 
+
+  private API = 'https://fakestoreapi.com/products';
+
+  buscarProduto(){
+    return this.http.get<ProdutoApi []>(this.API);
+
+  }
+  transformarProdutos(dados: ProdutoApi[]): Produto[]{
+    return dados.map((p) => ({
+        nome: p.title,
+        preco: p.price
+    }));
+  }
+}

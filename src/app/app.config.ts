95a1b877aecfe8e8c256057b22b3cd53663ad518
Marcos Vinicius <viniciusjunior4444@gmail.com>
2026-07-24 16:56:26 -0300

    feat: adicionei 2 import

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,6 +1,8 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideHttpClient, withFetch } from '@angular/common/http';
+import { withInterceptors } from '@angular/common/http';
+import { httpInterceptor} from './core/interceptors/http.interceptor';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
@@ -10,6 +12,8 @@ export const appConfig: ApplicationConfig = {
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes),
      provideClientHydration(),
-     provideHttpClient(withFetch()),
+     provideHttpClient(withFetch(),
+    withInterceptors([httpInterceptor])),
   ]
 };
+// criei 2 novos import

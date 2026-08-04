f6c8c4112affd945a190e4ffb9c6deace00100fa
Marcos Vinicius <viniciusjunior4444@gmail.com>
2026-08-04 17:05:55 -0300

    feat: novos imports

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,17 +1,19 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet, RouterLink } from '@angular/router';
 import { usuarioLogado, login, logout } from './core/auth';
+import { Header } from './shared/layout/header/header';
+import { MatAnchor } from '@angular/material/button';
 //import { Produto } from './features/produtos/produto/produto';
 //import { ListProdutos } from './features/produtos/list-produtos/list-produtos';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RouterLink],
+  imports: [RouterOutlet, RouterLink , Header,MatAnchor],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('e-commerce');
-  nomeLoja = 'Mini box são Francisco'; //nome do e-commerce
+  // nomeLoja = 'Mini box são Francisco'; //nome do e-commerce
   usuarioLogado = usuarioLogado;
   login = login;
   logout = logout;

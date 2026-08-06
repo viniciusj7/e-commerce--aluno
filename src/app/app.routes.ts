b886fe5f0851c55cf7ff0a6e148729d5dc2646cf
Marcos Vinicius <viniciusjunior4444@gmail.com>
2026-08-06 16:43:46 -0300

    feat:adicionei um checkout

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -41,6 +41,11 @@ export const routes: Routes = [
       import('./features/carrinho/carrinho/carrinho').then((m) => m.Carrinho),
   },
   {
+    path: 'checkout',
+    loadComponent:()=>
+      import('./features/checkout/Checkout/checkout').then((m) => m.Checkout),
+  },
+  {
     path:'**',
     redirectTo: '',
   },
